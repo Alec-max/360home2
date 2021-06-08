8459955e9f6d2525b5b499d85811838b1a945fd1
Alec-max <85555221+Alec-max@users.noreply.github.com>
2021-06-08 13:27:54 +0200

    init

---
+++ data.js
@@ -0,0 +1,623 @@
+var APP_DATA = {
+  "scenes": [
+    {
+      "id": "0-strae",
+      "name": "Straße",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1344,
+      "initialViewParameters": {
+        "yaw": -3.0985696700539815,
+        "pitch": -0.31880588982571645,
+        "fov": 1.4134061960355204
+      },
+      "linkHotspots": [
+        {
+          "yaw": -2.3039016169882025,
+          "pitch": -0.08404703542836245,
+          "rotation": 0,
+          "target": "1-flurwohnhauseg"
+        },
+        {
+          "yaw": 2.9466252937075055,
+          "pitch": -0.0591390600113364,
+          "rotation": 0,
+          "target": "9-flurmieterseite"
+        }
+      ],
+      "infoHotspots": [
+        {
+          "yaw": -2.4562644627513457,
+          "pitch": -0.4230360119731458,
+          "title": "Wohnhaus",
+          "text": "Text"
+        },
+        {
+          "yaw": 2.666818914898448,
+          "pitch": -0.49255310729740565,
+          "title": "Mietwohnung1",
+          "text": "Text"
+        }
+      ]
+    },
+    {
+      "id": "1-flurwohnhauseg",
+      "name": "FlurWohnhausEG",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1344,
+      "initialViewParameters": {
+        "pitch": 0,
+        "yaw": 0,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [
+        {
+          "yaw": -0.1109953588625352,
+          "pitch": -0.02267543472574829,
+          "rotation": 0,
+          "target": "0-strae"
+        },
+        {
+          "yaw": -3.057077096563308,
+          "pitch": 0.05636930340238422,
+          "rotation": 0,
+          "target": "2-hof"
+        },
+        {
+          "yaw": -1.9409685335071458,
+          "pitch": -0.8500397683090135,
+          "rotation": 0,
+          "target": "12-flurwohnhaus1etage"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "2-hof",
+      "name": "HOF",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1344,
+      "initialViewParameters": {
+        "yaw": 0.39659461333299006,
+        "pitch": -0.01056550824490543,
+        "fov": 1.4134061960355204
+      },
+      "linkHotspots": [
+        {
+          "yaw": 0.09849934028800433,
+          "pitch": -0.03522092984119496,
+          "rotation": 0,
+          "target": "6-terrasse"
+        },
+        {
+          "yaw": 1.4080963416309302,
+          "pitch": 0.014026358351536317,
+          "rotation": 0,
+          "target": "3-heizungsraumwohnhaus"
+        },
+        {
+          "yaw": 0.8509601720043083,
+          "pitch": 0.009098514372574584,
+          "rotation": 0,
+          "target": "5-werkstatt"
+        },
+        {
+          "yaw": 2.529634633967345,
+          "pitch": 0.019833410088558878,
+          "rotation": 0,
+          "target": "1-flurwohnhauseg"
+        },
+        {
+          "yaw": -2.7300182103279553,
+          "pitch": 0.02196624591092089,
+          "rotation": 0,
+          "target": "9-flurmieterseite"
+        },
+        {
+          "yaw": -3.0358557962569748,
+          "pitch": 0.08476168734068423,
+          "rotation": 0,
+          "target": "0-strae"
+        }
+      ],
+      "infoHotspots": [
+        {
+          "yaw": -1.4797684931845438,
+          "pitch": -0.5702693620839607,
+          "title": "Mietwohnung2",
+          "text": "Text"
+        },
+        {
+          "yaw": -1.7865216008559326,
+          "pitch": -0.062198463146335925,
+          "title": "Mietwohnung3",
+          "text": "Text"
+        }
+      ]
+    },
+    {
+      "id": "3-heizungsraumwohnhaus",
+      "name": "HeizungsraumWohnhaus",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1344,
+      "initialViewParameters": {
+        "yaw": -2.9116988322071915,
+        "pitch": -0.004216541088142733,
+        "fov": 1.4134061960355204
+      },
+      "linkHotspots": [],
+      "infoHotspots": []
+    },
+    {
+      "id": "4-gstewc",
+      "name": "Gästewc",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1344,
+      "initialViewParameters": {
+        "yaw": 0.7503966990314623,
+        "pitch": 0.03479993133669268,
+        "fov": 1.4134061960355204
+      },
+      "linkHotspots": [
+        {
+          "yaw": -2.0270988812346964,
+          "pitch": 0.13064977993910176,
+          "rotation": 0,
+          "target": "1-flurwohnhauseg"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "5-werkstatt",
+      "name": "Werkstatt",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1344,
+      "initialViewParameters": {
+        "yaw": 0.17772305343677708,
+        "pitch": 0.05653005604840011,
+        "fov": 1.4134061960355204
+      },
+      "linkHotspots": [
+        {
+          "yaw": 0.2666280214359418,
+          "pitch": 0.055637497198734565,
+          "rotation": 0,
+          "target": "2-hof"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "6-terrasse",
+      "name": "Terrasse",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1344,
+      "initialViewParameters": {
+        "yaw": 0.7073380667433096,
+        "pitch": 0.12049868219791549,
+        "fov": 1.4134061960355204
+      },
+      "linkHotspots": [
+        {
+          "yaw": 0.7692874318405742,
+          "pitch": 0.27033666274660817,
+          "rotation": 0,
+          "target": "2-hof"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "7-terrasse2",
+      "name": "Terrasse2",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1344,
+      "initialViewParameters": {
+        "yaw": 0.6172870422840866,
+        "pitch": -0.11742167470998766,
+        "fov": 1.4134061960355204
+      },
+      "linkHotspots": [
+        {
+          "yaw": -1.3291373696622024,
+          "pitch": 0.5041083911282911,
+          "rotation": 0,
+          "target": "6-terrasse"
+        },
+        {
+          "yaw": 1.3261995621803067,
+          "pitch": 0.026867040715551127,
+          "rotation": 0,
+          "target": "8-garten"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "8-garten",
+      "name": "Garten",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1344,
+      "initialViewParameters": {
+        "pitch": 0,
+        "yaw": 0,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [
+        {
+          "yaw": -0.49895898821368156,
+          "pitch": 0.20412170675435526,
+          "rotation": 0,
+          "target": "7-terrasse2"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "9-flurmieterseite",
+      "name": "FlurMieterseite",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1344,
+      "initialViewParameters": {
+        "pitch": 0,
+        "yaw": 0,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [
+        {
+          "yaw": -0.5005150702823897,
+          "pitch": -0.28136274360001146,
+          "rotation": 0,
+          "target": "10-flurmietwohnungen"
+        }
+      ],
+      "infoHotspots": [
+        {
+          "yaw": 1.531643289649014,
+          "pitch": 0.1635354593224676,
+          "title": "Zugang zu kommender dritter Wohnung",
+          "text": "Text"
+        }
+      ]
+    },
+    {
+      "id": "10-flurmietwohnungen",
+      "name": "FlurMietwohnungen",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1344,
+      "initialViewParameters": {
+        "yaw": -0.0684706091167051,
+        "pitch": 0.009189897243402356,
+        "fov": 1.4134061960355204
+      },
+      "linkHotspots": [
+        {
+          "yaw": 0.16112191648575802,
+          "pitch": 0.7091803728521846,
+          "rotation": 0,
+          "target": "9-flurmieterseite"
+        }
+      ],
+      "infoHotspots": [
+        {
+          "yaw": -0.6202631649395229,
+          "pitch": 0.14063919891494336,
+          "title": "Mietwohnung1",
+          "text": "Text"
+        },
+        {
+          "yaw": 2.7967492785792656,
+          "pitch": 0.03748792502175746,
+          "title": "Mietwohnung2",
+          "text": "Text"
+        }
+      ]
+    },
+    {
+      "id": "11-bad_1etage",
+      "name": "Bad_1Etage",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1344,
+      "initialViewParameters": {
+        "yaw": -2.5059405333858145,
+        "pitch": -0.12108516163832306,
+        "fov": 1.4134061960355204
+      },
+      "linkHotspots": [
+        {
+          "yaw": -0.8107756310799505,
+          "pitch": 0.12627149528694304,
+          "rotation": 0,
+          "target": "12-flurwohnhaus1etage"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "12-flurwohnhaus1etage",
+      "name": "FlurWohnhaus1Etage",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1344,
+      "initialViewParameters": {
+        "yaw": 0.002744913959766393,
+        "pitch": 0.18931188321410808,
+        "fov": 1.4134061960355204
+      },
+      "linkHotspots": [
+        {
+          "yaw": 0.5104621246814229,
+          "pitch": 0.05873758026400111,
+          "rotation": 0,
+          "target": "11-bad_1etage"
+        }
+      ],
+      "infoHotspots": [
+        {
+          "yaw": -2.120488130431605,
+          "pitch": 0.15456350843160926,
+          "title": "Kinderzimmer",
+          "text": "Text"
+        },
+        {
+          "yaw": 2.6877778039495634,
+          "pitch": 0.05610305937654303,
+          "title": "Kinderzimmer 2",
+          "text": "Text"
+        },
+        {
+          "yaw": -0.5650416929954538,
+          "pitch": 0.006984444610335316,
+          "title": "schlafzimmer",
+          "text": "Text"
+        },
+        {
+          "yaw": 0.2946914659300912,
+          "pitch": -0.06453991625767941,
+          "title": "ankleidezimmer<div><br></div>",
+          "text": "Text"
+        }
+      ]
+    }
+  ],
+  "name": "Project Title",
+  "settings": {
+    "mouseViewMode": "drag",
+    "autorotateEnabled": true,
+    "fullscreenButton": false,
+    "viewControlButtons": false
+  }
+};
